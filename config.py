cadef46926ff590d4082d2de532b5bcfea8c889c
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-10-28 23:37:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', 'YOUR_BOT_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 HOST = '0.0.0.0'
 PORT = 5000
-SECRET_KEY = os.getenv('SECRET_KEY', 'change_me')
+SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
 EMBED_BASE_URL = 'https://stream.futbol-x.site'
 REDIS_URL = 'redis://localhost:6379/0'
+SEGMENT_DIR = '/tmp/stream_segments'
